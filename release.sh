9de93dc2eaee5a4a2d515453cd9572d9267b92c5
Antoine Dupuis <moonshadws@gmail.com>
2025-05-11 22:41:11 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-zip -r mod_better_legends_tooltips.zip . -x 'release.sh' -x '*.git*'
+rm mod_better_legends_tooltips.zip
+zip -r mod_better_legends_tooltips.zip . -x 'release.sh' -x '*.git*' -x '*.zip*'
 
